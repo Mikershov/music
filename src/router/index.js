2142b4c9d7dd5f5e43916b171cd5d0ab3a84a45f
Mik <mik@mik.com>
2021-09-16 17:43:42 +0300

    main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/Home.vue'
 import Music from '../views/Music.vue'
 
 Vue.use(VueRouter)
@@ -9,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Music
   },
   {
     path: '/about',
