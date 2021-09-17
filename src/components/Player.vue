629f0b9a5f166973900ba8b01f3f13bac1f43d45
Oleg Belov <crazyxoma@gmail.com>
2021-09-17 12:01:48 +0300

    mainline vol 0.2

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -157,7 +157,7 @@ export default {
   mounted() {
 
     var vid = document.getElementById('mainLine_' + this.genreName);
-    vid.volume = 0.45;
+    vid.volume = 0.2;
 
     this.auCon = new AudioContext;
 
